366fa453a639ce2907cb3218ce4f1558041e9a62
Vincenzo Aceto <vincenzo.aceto@nttdata.com>
2017-12-14 23:44:32 +0100

    Initial Commit

---
+++ GitTest3Tests/GitTest3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GitTest3Tests.swift
+//  GitTest3Tests
+//
+//  Created by Vincenzo Aceto on 14/12/2017.
+//  Copyright © 2017 Vincenzo Aceto. All rights reserved.
+//
+
+import XCTest
+@testable import GitTest3
+
+class GitTest3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
